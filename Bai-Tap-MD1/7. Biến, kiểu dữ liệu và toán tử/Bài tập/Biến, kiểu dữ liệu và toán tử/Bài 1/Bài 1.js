b91ede2ca2abf879ccf093c108cda4f58884a058
Vlynn01 <89231237+Vlynn01@users.noreply.github.com>
2022-08-07 13:47:17 +0700

    Add files via upload

---
+++ Bai-Tap-MD1/7. Biến, kiểu dữ liệu và toán tử/Bài tập/Biến, kiểu dữ liệu và toán tử/Bài 1/Bài 1.js
@@ -0,0 +1,11 @@
+var vatly = 10;
+var hoahoc = 10;
+var sinhhoc = 10;
+
+var diemtrungbinh3mon = (vatly + hoahoc + sinhhoc) / 3;
+var tongdiem3mon = vatly + hoahoc + sinhhoc;
+
+document.write("Điểm trung bình 3 môn của Luận là:" + diemtrungbinh3mon);
+
+
+document.write("Tổng điểm 3 môn của Luận là:" + tongdiem3mon);
---
+++ Bai-Tap-MD1/7. Biến, kiểu dữ liệu và toán tử/Bài tập/Biến, kiểu dữ liệu và toán tử/Bài 2/Bài 2.js
@@ -0,0 +1,18 @@
+var C = 0;
+
+var F = 0;
+
+var CtoF = 9 / 5 * C + 32;
+
+var FtoC = 5 / 9 *(F - 32);
+
+document.write("chuyển từ độ C sang độ F"  + CtoF ) ;
+
+
+
+
+document.write("chuyển đổi từ độ F sang độC" + FtoC);
+
+
+
+
