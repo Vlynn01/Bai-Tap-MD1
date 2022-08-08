34f7d43a02e2e3f7213859dcfbf7654c247516ae
Vlynn01 <89231237+Vlynn01@users.noreply.github.com>
2022-08-08 19:50:33 +0700

    Add files via upload

---
+++ Bai-Tap-MD1/7. Biến, kiểu dữ liệu và toán tử/Bài tập/Biến, kiểu dữ liệu và toán tử/Bài 3/Bài 3.js
@@ -0,0 +1,8 @@
+const pi=3.14;
+
+nhapBanKinhHinhTron=prompt("Mời bạn nhập bán kính");
+let r =parseInt(nhapBanKinhHinhTron);
+
+let S= pi*r*r;
+
+document.write("Diện tích hình tròn" +S);
---
+++ Bai-Tap-MD1/7. Biến, kiểu dữ liệu và toán tử/Bài tập/Biến, kiểu dữ liệu và toán tử/Bài 4/Bài 4.js
@@ -0,0 +1,8 @@
+const pi=3.14;
+
+nhapBanKinhHinhTron=prompt("Mời bạn nhập bán kính");
+let r =parseInt(nhapBanKinhHinhTron);
+
+let C = 2 * pi * r;
+
+document.write("Chu vi hình tròn" +C);
